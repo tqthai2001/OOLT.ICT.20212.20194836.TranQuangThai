67212c241f3f4b1d0aaeb5bf21a099360c79a237
Tran Quang Thai <tqthai2001@gmail.com>
2022-04-15 00:47:27 +0700

    Second Commit

--- Lab03/AimsProject/src/Aims.java
+++
@@ -1,7 +0,0 @@
-public class Aims {
-    public static void main(String[] args) {
-        Cart anOrder = new Cart();
-        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King",
-                "Animation", "Roger Allers", 87, 19.95f);
-    }
-}
