7f41db0b7d1b1ed0a59412dacdab39235e3fe887
Tran Quang Thai <tqthai2001@gmail.com>
2022-04-20 17:31:37 +0700

    First Commit

---
+++ Lab04/AimsProject/src/aims/TestPassingParameter.java
@@ -0,0 +1,27 @@
+package aims;
+
+public class TestPassingParameter {
+	public static void main(String[] args) {
+		DigitalVideoDisc jungleDVD = new DigitalVideoDisc("Jungle");
+		DigitalVideoDisc cinderellaDVD = new DigitalVideoDisc("Cinderella");
+		
+		swap(jungleDVD, cinderellaDVD);
+		System.out.println("Jungle DVD Title: " + jungleDVD.getTitle());
+		System.out.println("Cinderella DVD Title " + cinderellaDVD.getTitle());
+		
+		changeTitle(jungleDVD, cinderellaDVD.getTitle());
+		System.out.println("Jungle DVD Title " + jungleDVD.getTitle());
+	}
+	
+	public static void swap(Object o1, Object o2) {
+		Object tmpObject = o1;
+		o1 = o2;
+		o2 = tmpObject;
+	}
+	
+	public static void changeTitle(DigitalVideoDisc dvd, String title) {
+		String oldTitle = dvd.getTitle();
+		dvd.setTitle(title);
+		dvd = new DigitalVideoDisc(oldTitle);
+	}
+}
